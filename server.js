b92ffc001b239c2c3b11dd0fd1bc3bd03215724a
Tavilo Breno <breno_wk2@hotmail.com>
2022-04-25 00:05:07 -0300

    Criação da api de produtos

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//Iniciando o app
+const app = express();
+app.use(express.json());
+
+
+//Iniciando o banco de dados
+mongoose.connect("mongodb://localhost:27017/nodeapi", 
+{ useNewUrlParser: true }
+);
+
+requireDir("./src/models");
+
+//const Product = mongoose.model("Product");
+//Rotas
+app.use('/api', require("./src/routes"));
+
+app.listen(3001);
